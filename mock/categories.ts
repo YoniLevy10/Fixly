309c210f6d5ef10153c1911bfb302a407ace24ae
Yoni Levy <levyyoni5@gmail.com>
2026-05-26 22:33:11 +0300

    Add marketplace categories mock

---
+++ mock/categories.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  {
+    id: 'cleaning',
+    name: 'Home Cleaning',
+  },
+  {
+    id: 'electrician',
+    name: 'Electrician',
+  },
+  {
+    id: 'plumbing',
+    name: 'Plumber',
+  },
+]
