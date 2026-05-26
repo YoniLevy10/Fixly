44ca83620925feb336552f21f33a58187f7487cf
Yoni Levy <levyyoni5@gmail.com>
2026-05-26 22:43:16 +0300

    Add preferred time options

---
+++ lib/request/request-time-options.ts
@@ -0,0 +1,14 @@
+export const requestTimeOptions = [
+  {
+    id: 'morning',
+    label: 'Morning',
+  },
+  {
+    id: 'afternoon',
+    label: 'Afternoon',
+  },
+  {
+    id: 'evening',
+    label: 'Evening',
+  },
+]
