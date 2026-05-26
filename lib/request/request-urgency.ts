9b555588146405c97bf60c5a3639475ccee62c93
Yoni Levy <levyyoni5@gmail.com>
2026-05-26 22:37:38 +0300

    Add request urgency options

---
+++ lib/request/request-urgency.ts
@@ -0,0 +1,14 @@
+export const requestUrgencyOptions = [
+  {
+    id: 'asap',
+    label: 'As soon as possible',
+  },
+  {
+    id: 'today',
+    label: 'Later today',
+  },
+  {
+    id: 'scheduled',
+    label: 'Schedule for another time',
+  },
+]
