169fd39ff10fda6531a987d182365204dafc7e39
Yoni Levy <levyyoni5@gmail.com>
2026-05-26 21:07:21 +0300

    Add centralized application routes

---
+++ shared/constants/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  HOME: '/',
+
+  CATEGORIES: '/categories',
+  PROFESSIONALS: '/professionals',
+
+  PROFESSIONAL_PROFILE: '/professionals/[id]',
+
+  REQUEST_NEW: '/requests/new',
+  REQUEST_DETAILS: '/requests/[id]',
+
+  DASHBOARD: '/dashboard',
+  DASHBOARD_REQUESTS: '/dashboard/requests',
+
+  ADMIN: '/admin',
+} as const
