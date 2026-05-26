0794b1c76aca6ff73beac54b9fbf8038a6a8c68a
Yoni Levy <levyyoni5@gmail.com>
2026-05-26 22:36:33 +0300

    Add recommendation marketplace sections

---
+++ mock/recommendation-sections.ts
@@ -0,0 +1,14 @@
+export const recommendationSections = [
+  {
+    id: 'top-rated',
+    title: 'Top Rated Professionals',
+  },
+  {
+    id: 'available-today',
+    title: 'Available Today',
+  },
+  {
+    id: 'quick-response',
+    title: 'Fastest Response Time',
+  },
+]
